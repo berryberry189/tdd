e985c0edffb9b8f569ae4486608d5bdf34a42860
berryberry189 <berryberry189@naver.com>
2021-12-17 17:47:58 +0900

    Refactor PasswordStrengthMeter

--- src/test/java/com/book/tdd/chap02/PasswordStrengthMeter.java
+++ src/test/java/com/book/tdd/chap02/PasswordStrengthMeter.java
@@ -3,17 +3,14 @@ package com.book.tdd.chap02;
 public class PasswordStrengthMeter {
     public PasswordStrength meter(String str){
         if(str == null || str.isEmpty()) return PasswordStrength.INVALID;
-        boolean lengthEnough = str.length() >= 8;
-        boolean containsNum = meetsContainingNumberCriteria(str);
-        boolean containsUpp = meetsContainingUppercaseCriteria(str);
+        int metCounts = 0;
 
-        if(lengthEnough && !containsNum && !containsUpp) return PasswordStrength.WEAK;
-        if(!lengthEnough && containsNum && !containsUpp) return PasswordStrength.WEAK;
-        if(!lengthEnough && !containsNum && containsUpp) return PasswordStrength.WEAK;
+        if(str.length() >= 8) metCounts ++;
+        if(meetsContainingNumberCriteria(str)) metCounts ++;
+        if(meetsContainingUppercaseCriteria(str)) metCounts ++;
 
-        if(!lengthEnough) return PasswordStrength.NORMAL;
-        if(!containsNum) return PasswordStrength.NORMAL;
-        if(!containsUpp) return PasswordStrength.NORMAL;
+        if(metCounts == 1) return PasswordStrength.WEAK;
+        if(metCounts == 2) return PasswordStrength.NORMAL;
 
         return PasswordStrength.STRONG;
     }
