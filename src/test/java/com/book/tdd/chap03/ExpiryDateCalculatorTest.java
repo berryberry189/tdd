80d4571d051c6f9ff437947afe3722f4ca584e77
berryberry189 <berryberry189@naver.com>
2021-12-18 20:17:48 +0900

    Test 납부일과_한달_뒤_일자가_같지_않음 예외 추가

--- src/test/java/com/book/tdd/chap03/ExpiryDateCalculatorTest.java
+++ src/test/java/com/book/tdd/chap03/ExpiryDateCalculatorTest.java
@@ -11,11 +11,20 @@ public class ExpiryDateCalculatorTest {
 
     @Test
     void 만원_납부하면_한달_뒤가_만료일이_됨(){
-
         assertExpiryDate(LocalDate.of(2021, 12, 18), 10000,
                 LocalDate.of(2022, 1, 18));
         assertExpiryDate(LocalDate.of(2021, 11, 18), 10000,
                 LocalDate.of(2021, 12, 18));
+    }
+
+    @Test
+    void 납부일과_한달_뒤_일자가_같지_않음(){
+        assertExpiryDate(LocalDate.of(2021, 1, 31), 10000,
+                LocalDate.of(2021, 2, 28));
+        assertExpiryDate(LocalDate.of(2021, 5, 31), 10000,
+                LocalDate.of(2021, 6, 30));
+        assertExpiryDate(LocalDate.of(2020, 1, 31), 10000,
+                LocalDate.of(2020, 2, 29));
     }
 
     private void assertExpiryDate(LocalDate billingDate, int payAmount, LocalDate expectedExpiryDate){
